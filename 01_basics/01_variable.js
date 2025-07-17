aec8a170e305c40a9b5754075d126996e1dbbf14
Bhooveshvyas <vyasbhoovesh@gmail.com>
2025-07-18 00:22:59 +0530

    NEW

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,10 @@
+const accountId = 20040508
+let accountMail = "vyasbhoovesh@gmailcom"
+// we will use let not "var" bcz of scope of the variable
+var accountPassword = "Bhooiiii"
+accountOwner = "Bhoovesh Vyas"
+// accountId=4
+console.log(accountPassword);
+console.log(accountId);
+console.log(accountMail);
+console.table([accountPassword, accountId, accountMail, accountOwner]);
---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,14 @@
+"use stict"; // treat all js code as the newer version
+alert(3 + 3); // we are using nodejs not browser
+
+console.log("BHOOVESH");
+let n = 10;
+// data type 01 = number {range = [-2^53-1,2^53-1]} (big int is used for bigger numbers)
+let name = 10;
+// data type 02 = string can be used in single as well as in double quote
+let flag = true;
+// data type 03 = boolean (true/false)
+let nll = null;
+// data type 04 = null(object type)(stand alone value))
+let k;
+// data type 05 = undefined
