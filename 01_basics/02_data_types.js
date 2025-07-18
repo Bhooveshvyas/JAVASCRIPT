422260ab033ad1f3dd3eafe9c23553aed2e4d1ed
Bhooveshvyas <vyasbhoovesh@gmail.com>
2025-07-18 20:14:36 +0530

    NEW

--- 01_basics/02_data_types.js
+++ 01_basics/02_data_types.js
@@ -1,14 +1,20 @@
-"use stict"; // treat all js code as the newer version
-alert(3 + 3); // we are using nodejs not browser
+// "use stict"; // treat all js code as the newer versbion
+// alert(3 + 3); // we are using nodejs not browser
 
 console.log("BHOOVESH");
 let n = 10;
 // data type 01 = number {range = [-2^53-1,2^53-1]} (big int is used for bigger numbers)
-let name = 10;
+let name = "10";
 // data type 02 = string can be used in single as well as in double quote
 let flag = true;
 // data type 03 = boolean (true/false)
 let nll = null;
-// data type 04 = null(object type)(stand alone value))
+// data type 04 = null = stand alone value (object type)
 let k;
 // data type 05 = undefined
+// symbol =>unique identifier
+console.log(typeof n)
+console.log(typeof name)
+console.log(typeof flag)
+console.log(typeof nll)
+console.log(typeof k)
