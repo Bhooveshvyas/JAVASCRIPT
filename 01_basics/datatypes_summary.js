8c3bf73a3789664b9e7f9b009ed13ba46fc731e4
Bhooveshvyas <vyasbhoovesh@gmail.com>
2025-07-19 23:06:46 +0530

    SUMMARY

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,31 @@
+// primitive data type
+//  7 types : string, number, boolean, null, undefined, symbol, bigint
+
+// non-primitive(reference) data type
+//  1 type : object
+//  object can be further divided into:
+//    - object literal
+//    - array
+//    - function
+
+// array =>
+const heros = ["thor", "ironman", "hulk"];
+
+// object =>
+const person = {
+  name: "John",
+  age: 30,
+  isEmployed: true,
+};
+
+// function =>
+// fuction can be declared in two ways:
+function greet() {
+  console.log("Hello, World!");
+}
+
+const func = function () {
+  console.log("HELLOO JIII");
+};
+
+// type of non-primitive data type->function{bas function ka type hai object-fuction}
